69817570cbbd28e898d36dfabb57a66a319d7af3
Grigory Malanchuk <grigorymalanchuk@gmail.com>
2024-06-17 21:18:09 +0300

    created the role model for sensitive customization of user permissions

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from tasks.models import Board, Task, Column
+from tasks.models import Board, Task, Column, BoardMemberRelation, Role
 
 
 @admin.register(Board)
@@ -15,4 +15,14 @@ class ColumnAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BoardMemberRelation)
+class BoardMemberRelationAdmin(admin.ModelAdmin):
     pass
--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -10,17 +10,17 @@ class AbsTimeStampModel(models.Model):
 
 
 class Board(AbsTimeStampModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
     description = models.CharField(max_length=512, null=True, blank=True)
     owner = models.ForeignKey(to="users.User", on_delete=models.CASCADE, related_name="boards")
-    # TODO users = пользователи, у которых есть доступ к этой доске (могут создавать, редактировать таски, колонки)
+    members = models.ManyToManyField(to="users.User", through="BoardMemberRelation", related_name="member_boards", verbose_name="Users who have access to the board. Users have different roles")
 
     def __str__(self):
         return f"ID: {self.pk} | Name: {self.name}"
 
 
 class Column(AbsTimeStampModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=128)
     description = models.CharField(max_length=256, null=True, blank=True)
     board = models.ForeignKey(to=Board, on_delete=models.CASCADE, related_name="columns")
     owner = models.ForeignKey(to="users.User", on_delete=models.CASCADE, related_name="columns")
@@ -41,3 +41,35 @@ class Task(AbsTimeStampModel):
 
     def __str__(self):
         return f"ID: {self.pk} | Description: {self.description[:15]}..."
+
+
+class BoardMemberRelation(AbsTimeStampModel):
+    user = models.ForeignKey(to="users.User", on_delete=models.CASCADE, related_name="board_users")
+    board = models.ForeignKey(to="Board", on_delete=models.CASCADE, related_name="board_users")
+    role = models.ForeignKey(to="Role", on_delete=models.CASCADE, related_name="board_users")
+
+    def __str__(self):
+        return f"ID: {self.pk} | User: {self.user} | Board: {self.board} | Role: {self.role}"
+
+
+class Role(AbsTimeStampModel):
+    # TODO when creating a board, four roles will be created by default (guest, user, moderator, administrator)
+    # TODO create additional fields for more thorough customization
+
+    name = models.CharField(max_length=64)
+
+    # GUEST. all fields are nullable. can only view the board and tasks
+
+    # USER
+    can_drag_tasks = models.BooleanField(default=False, verbose_name="Can drag tasks between columns")
+    can_manage_own_tasks = models.BooleanField(default=False, verbose_name="Can create, edit and delete only own tasks")
+
+    # MODERATOR
+    can_manage_all_tasks = models.BooleanField(default=False, verbose_name="Can edit and delete all tasks")
+
+    # ADMINISTRATOR
+    can_manage_columns = models.BooleanField(default=False, verbose_name="Can create, edit and delete all columns")
+    can_manage_board = models.BooleanField(default=False, verbose_name="Can edit board but not delete it")
+
+    def __str__(self):
+        return f"ID: {self.pk} | Name: {self.name}"
